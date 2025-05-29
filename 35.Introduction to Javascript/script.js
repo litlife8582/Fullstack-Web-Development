d41b2bf294dea5dc0679c82796f148cec0a60e8d
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-05-29 18:10:02 +0530

    done

---
+++ 35.Introduction to Javascript/script.js
@@ -0,0 +1,14 @@
+alert("Hello World")
+
+console.log("code is running")
+
+
+var a=prompt("Enter your number")
+
+console.log("Your number is "+ a)
+
+
+//Changing document title
+document.title="Hey I am good"
+
+document.body.style.backgroundColor="blue"
