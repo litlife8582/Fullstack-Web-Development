a78a858a182c11a4de98f687991dfce2210fd7cc
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-05-30 11:46:27 +0530

    done

--- 40.Javascript Strings/script.js
+++ 40.Javascript Strings/script.js
@@ -1,1 +1,29 @@
-console.log(Math.random());
+let a="Mayukh"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+console.log(a.length)
+
+let name="Mayukh";
+let friend="Tamaghna"
+
+console.log("My name is "+name+" and my friend's name is "+friend)
+console.log(`My name is ${name} and my friend's name is ${friend}`)//string interpolation
+
+
+let b="Disha"
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1,2));
+console.log(b.slice(1,));
+console.log(b.replace('s','h'));
+console.log(b.concat("Deepshikha","Arnab"));
+
+//Strings are immutable
+
