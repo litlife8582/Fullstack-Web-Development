588e6b125d9944d941ace73b00395033de5c14f3
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-06-03 19:47:20 +0530

    done

---
+++ Exercise 13- Website builder/script.js
@@ -0,0 +1,39 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+
+    const container=document.getElementById("card-container");
+    const card=document.createElement("div");
+    card.className="card";
+
+    card.innerHTML=`
+    <img src="${thumbnail}" alt="${title}">
+    <div class="card-content">
+        <div class="card-title">${title}</div>
+        <div class="card-meta">${cName} • ${views} • ${monthsOld} months ago</div>
+        <div class="card-meta">${duration}</div>
+    `;
+
+    container.appendChild(card);
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+
+// function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+//     const container = document.getElementById("card-container");
+
+//     const card = document.createElement("div");
+//     card.className = "card";
+
+//     card.innerHTML = `
+//         <img src="${thumbnail}" alt="${title}">
+//         <div class="card-content">
+//             <div class="card-title">${title}</div>
+//             <div class="card-meta">${cName} • ${views} views • ${monthsOld} months ago</div>
+//             <div class="card-meta">⏱ ${duration}</div>
+//         </div>
+//     `;
+
+//     container.appendChild(card);
+// }
