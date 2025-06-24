b54652ed5eeec098e9d0cb707b4306dc0212de44
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-06-24 19:36:46 +0530

    done

--- 57.Middlewares in Express Js/main.js
+++ 57.Middlewares in Express Js/main.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs=require("fs")
+
+app.use(express.static("public"))
+
+const middleware = function(req, res, next) {
+  fs.appendFileSync("log.txt",`${Date.now()} and ${req.method}\n`)
+  console.log(`${Date.now()} and ${req.method}`)
+  // res.send("Hacked by the middleware")
+  next()
+}
+
+app.use(middleware)//logger for the website
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
