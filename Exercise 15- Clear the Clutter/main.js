77c0af4f3ee2f873b6660ec59bcca616f3c5223c
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-06-26 11:54:52 +0530

    done

--- Exercise 15- Clear the Clutter/main.js
+++ Exercise 15- Clear the Clutter/main.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fs= require("fs")
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const basePath = "C:\\Users\\Mayuk\\Fullstack\\Exercise 15- Clear the Clutter";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+let files = await fs.readdir(basePath);
+
+console.log(files);
+
+for (const item of files) {
+
+  let ext = item.split(".")[item.split(".").length - 1];
+  console.log(ext);
+  
+  if (item.split(".").length > 1 && ext != "js" && ext != "json") {
+    if (fsn.existsSync(path.join(basePath, ext))) {
+      fs.rename(path.join(basePath, item), path.join(basePath, ext, item));
+    } else {
+      fs.mkdir(ext);
+    }
+  }
+  console.log(item);
+}
