16fc40d567f231c60e93e0a2c32a75ff30b4e00e
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-08-30 22:51:39 +0530

    updated

--- React/01viteapp/src/main.jsx
+++ React/01viteapp/src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {jsx as _jsx} from "react/jsx-runtime.js"
 
-function MyApp(){
+/*function MyApp(){
   return(
     <div>
       <h1>Custom App !!</h1>
     </div>
   )
-}
+}*/
+// MyApp() warning: Fast refresh only works when a file has exports. Move your component(s) to a separate file.
 
 // const reactElement = {
 
@@ -24,11 +25,15 @@ function MyApp(){
 
 // ^ was my custom render but to use react we have to render according to the necessities of react
 
-const anotherElement=(
+/*const anotherElement=(
   <a href="http://google.com" target='_blank'>
     Visit Error
   </a>
-)//A random object which can be rendered directly without tags
+)
+*/
+//warning: anotherElement" is assigned a value but never used.
+// anotherELement- A random object which can be rendered directly without tags
+
 
 const anotherUser="chai air react"
 
