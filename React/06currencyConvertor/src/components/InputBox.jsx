7d6bf02ad8e8e3c51417168265f185e1154e7881
Mayukh Sarkar <sarkarmayukh2006@gmail.com>
2025-09-06 01:38:34 +0530

    Currency Converter (with faulty fetch api)

---
+++ React/06currencyConvertor/src/components/InputBox.jsx
@@ -0,0 +1,57 @@
+import React,{useId} from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions=[],
+    selectCurrency="usd",
+    amountDisable=false,
+    currencyDisable=false,
+
+    
+    className = "",
+}) {
+   
+  const amountInputId=useId()
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}//runs if onAmountChange exists(value is passed) as no default value is provided
+                    //Number converts the string value e.target.value to number for onAmountChange to execute(it takes numeric value not string)
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                    
+                        {currencyOptions.map((currency)=>(
+                          <option key={currency} value={currency}>//Key has to be used in .jsx file to improve performance
+                            {currency}
+                          </option>
+                        ))}
+                
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
